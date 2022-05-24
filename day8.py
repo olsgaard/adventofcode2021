3497733b25546261615fdfc302819ab48c509b54
Mads Ølsgaard <mads@olsgaard.dk>
2022-05-24 22:06:40 +0400

    finish second task

--- day8.py
+++ day8.py
@@ -13,6 +13,11 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 '''
 
+test2 = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+
+test2_solution = 5353
+test_solutions =[8394,9781,1197,9361,4873,8418,4548,1625,8717,4315]
+
 def parse(digits):
 	rows = digits.strip().split('\n')
 	data = []
@@ -29,19 +34,6 @@ segment_count_to_digits = {
 	3: 7,
 	4: 4,
 	7: 8,
-}
-
-digits_to_segment_letter = {
-	0: 'abcef',
-	1: 'cf',
-	2: 'acdeg',
-	3: 'acdfg',
-	4: 'bcdf',
-	5: 'abdfg',
-	6: 'abdefg',
-	7: 'acf',
-	8: 'abcdefg',
-	9: 'abcdfg',
 }
 
 def solve1(data):
@@ -62,3 +54,52 @@ with open('input_day8.txt') as f:
 
 print(solve1(raw_data))
 
+scrable_to_clean = {}
+
+def get_number_by_length(length, scrambled):
+	return [set(el) for el in scrambled if len(el) == length]
+
+def parse_line(line):
+	line = [set(el) for el in line.strip().replace('|','').split()]
+	signal_patterns, output_values = line[:-4], line[-4:]
+	return signal_patterns, output_values
+
+def decode_line(line):
+	signal_patterns, output_values = parse_line(line)
+
+	one, = get_number_by_length(2, signal_patterns)
+	four, = get_number_by_length(4, signal_patterns)
+	seven, = get_number_by_length(3, signal_patterns)
+	eight = set('abcdefg')
+
+	two_three_five = get_number_by_length(5, signal_patterns)
+	three, = [el for el in two_three_five if one.issubset(el)]
+	two_five = [el for el in two_three_five if el != three]
+
+	eb = [el - three for el in two_five]
+
+	b, = [el for el in eb if el.issubset(four)]
+	e, = [el for el in eb if not el.issubset(four)]
+
+	two, = [el for el in two_five if e.issubset(el)]
+	five, = [el for el in two_five if b.issubset(el)]
+
+	six_nine_zero = get_number_by_length(6, signal_patterns)
+	nine, = [el for el in six_nine_zero if not e.issubset(el)]
+	six_zero = [el for el in six_nine_zero if el != nine]
+	zero, = [el for el in six_zero if seven.issubset(el)]
+	six, = [el for el in six_zero if el != zero]
+
+	solution = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+	digits = int(''.join([str(solution.index(val)) for val in output_values]))
+	return digits
+
+assert decode_line(test2) == test2_solution
+
+def solve(signals):
+	return sum([decode_line(line) for line in signals.strip().split('\n')])
+
+assert solve(test) == sum(test_solutions)
+
+print(solve(raw_data))
